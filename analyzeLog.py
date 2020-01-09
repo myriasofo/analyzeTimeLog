73a33d46af001d0e8f3c20970100d01781812b57
Abe <myriasofo@gmail.com>
2020-01-09 16:50:29 -0500

    Update dropbox to googledrive

--- analyzeLog.py
+++ analyzeLog.py
@@ -337,7 +337,7 @@ class EventsCalculator:
 def main():
     ## Setting up
     #logPath = os.path.expanduser('~/dev/analyzeLog/timeLog_example')
-    logPath = os.path.expanduser('~/Dropbox/Notes/_specials/timeLog.to')
+    logPath = os.path.expanduser('~/GoogleDrive/Notes/_specials/timeLog.to')
 
     e = Events()
     e.extractEventFromLog(logPath)
